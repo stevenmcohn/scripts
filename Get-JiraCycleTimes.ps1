cd6642adfd2de71d1b7bb46a53d8afc8da4180b0
stevenmcohn <steven_cohn@waters.com>
2023-12-14 21:09:43 -0500

    Get-PullRequests

--- Get-JiraCycleTimes.ps1
+++ Get-JiraCycleTimes.ps1
@@ -51,6 +51,8 @@ param (
 
 Begin
 {
+	. $PSScriptRoot\common.ps1
+
 	# SETTINGS...
 
 	$OutputFile = './issues.csv'
@@ -64,113 +66,9 @@ Begin
 	$EndStatus = 'Verified'
 
 
-	function AppendToPowerShellProfile
-	{
-		param($key, $command)
-		# PowerShell >= 6.0
-		$0 = "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
-		if (!(Test-Path $0)) { New-Item $0 -Force -Confirm:$false | Out-Null }
-		if ((Get-Content $0 | select-String $key).Count -eq 0) { Add-Content $0 $command }
-		# PowerShell <= 5.1
-		$0 = "$HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
-		if (!(Test-Path $0)) { New-Item $0 -Force -Confirm:$false | Out-Null }
-		if ((Get-Content $0 | select-String $key).Count -eq 0) { Add-Content $0 $command }
-	}
-
-	function CountWeekDays
-	{
-		param($start, $end)
-		$days = ($end - $start).TotalDays
-		for ($d = $start;$d -le $end; $d = $d.AddDays(1)) {
-			if ($d.DayOfWeek -match "Sunday|Saturday" -and $days -gt 0) {
-				$days -= 1.0
-			}
-		}
-		return $days
-	}
-	
-	function InstallChocolatey
-	{
-		# Modules/Scripts contains a better version but this is a stand-alone copy for the
-		# top-level Install scripts so they can remain independent of the Module scripts
-		if ((Get-Command choco -ErrorAction:SilentlyContinue) -eq $null)
-		{
-			# touch $profile prior to install
-			if (!(Test-Path $profile))
-			{
-				$folder = [System.IO.Path]::GetDirectoryName($profile)
-				if (!(Test-Path $folder)) { New-Item $folder -Itemtype Directory -Force -Confirm:$false | Out-Null }
-				New-Item -Path $profile -ItemType File -Value '' -Confirm:$false | Out-Null
-			}
-	
-			Write-Host '... Installing Chocolatey'
-			Set-ExecutionPolicy Bypass -Scope Process -Force
-			Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-	
-			AppendToPowerShellProfile 'chocolateyProfile.psm1' 'Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1'
-			. $profile
-		}
-	
-		# import profile to ensure we can use refreshenv
-		$0 = 'C:\ProgramData\chocolatey\helpers\chocolateyProfile.psm1'
-		if (Test-Path $0)
-		{
-			Import-Module $0
-		}
-	}
-
-	function InstallCurl
-	{
-		# default PowerShell curl alias points to Invoke-WebRequest which is... disappointing
-		Remove-Item alias:curl -ErrorAction:SilentlyContinue
-	
-		$cmd = Get-Command curl -ErrorAction:SilentlyContinue
-		if ($cmd -ne $null)
-		{
-			if ($cmd.Source.Contains('curl.exe')) { return }
-		}
-	
-		if ((Get-Command choco -ErrorAction:SilentlyContinue) -eq $null)
-		{
-			InstallChocolatey
-		}
-	
-		if ((choco list -l 'curl' | Select-string 'curl ').count -gt 0) { return }
-	
-		Write-Host '... Installing Curl'
-		choco install -y curl
-	
-		AppendToPowerShellProfile 'alias:curl' 'Remove-Item alias:curl -ErrorAction SilentlyContinue'
-		. $profile
-	}
-	
-	function PromptForValue
-	{
-		param($prompt, $value)
-		if ([String]::IsNullOrWhiteSpace($value))
-		{
-			While ([String]::IsNullOrWhiteSpace($value))
-			{
-				$value = Read-Host $prompt
-			}
-		}
-		else
-		{
-			$val = Read-Host "$prompt [$value]"
-			if (![String]::IsNullOrWhiteSpace($val))
-			{
-				$value = $val
-			}
-		}
-
-		return $value
-	}
-
-	# ==================================================================================
-
 	function GetIssues
 	{
-		'Team,Epic,Key,Points,StartedDt,InTestDt,PassedDt,VerifiedDt,Days,WeekDays,InProgress,InTest,Passed,Reworked,Repassed,Reverified' | Out-File -FilePath $File
+		'Team,User,Epic,Key,Points,StartedDt,InTestDt,PassedDt,VerifiedDt,Days,WeekDays,InProgress,InTest,Passed,Reworked,Repassed,Reverified' | Out-File -FilePath $File
 
 		Write-Host 'Legend: [.] OK, [+] reverified, [-] skip no start or end, [x] skip no points'
 		Write-Host
@@ -216,6 +114,7 @@ Begin
 		
 		$points = $issue.fields.$PointsField
 		$team = $issue.fields.$TeamField.Value
+		$user = $issue.fields.assignee.displayName
 		if ([String]::IsNullOrWhiteSpace($points))
 		{
 			# indicates that the story points are unspecified for this issue
@@ -290,7 +189,7 @@ Begin
 
 		Write-Host $marker -NoNewline
 
-		"$team,$epic,$($issue.Key),$points,$started,$tested,$passed,$finished,$days,$weekdays,$progress,$testedDays,$passedDays,$reworked,$repassed,$reverified" | Out-File -FilePath $File -Append
+		"$team,$user,$epic,$($issue.Key),$points,$started,$tested,$passed,$finished,$days,$weekdays,$progress,$testedDays,$passedDays,$reworked,$repassed,$reverified" | Out-File -FilePath $File -Append
 	}
 
 	function GetChangeLog
---
+++ Get-PullRequests.ps1
@@ -0,0 +1,177 @@
+<#
+.SYNOPSIS
+Export CSV of Github pull requests, including number of comments, commits, etc.
+
+.PARAMETER Append
+Specify this switch to append to the output file rather than recreate it.
+Makes it easy to append multiple users to a single file.
+
+.PARAMETER File
+The path of the output CSV file, default is ./issues.csv
+
+.PARAMETER Org
+The Github organization name.
+Override with $env:GH_ORG
+
+.PARAMETER Token
+Your personal Github access token, created from your Github account.
+Override with $env:GH_TOKEN
+
+.PARAMETER User
+The Github user to report
+
+.PARAMETER Username
+Your Github account username used for authentication.
+Override with $env:GH_USERNAME
+
+.DESCRIPTION
+blah
+#>
+
+# CmdletBinding adds -Verbose functionality, SupportsShouldProcess adds -WhatIf
+[CmdletBinding(SupportsShouldProcess = $true)]
+
+param (
+	[string] $Org,
+	[string] $Username,
+	[string] $Token,
+	[string] $User,
+	[string] $File,
+	[swtich] $Append
+)
+
+Begin
+{
+	. $PSScriptRoot\common.ps1
+
+	# SETTINGS...
+
+	$OutputFile = './pull-requests.csv'
+	$URI = 'https://api.github.com'
+	$Header = 'Accept: application/json'
+
+
+	function GetPullRequests
+	{
+		if (-not $Append) {
+			'User,Repo,PR,State,Commits,CComments,PComments,Created,Closed,Days' | Out-File -FilePath $File
+		}
+
+		Write-Host
+
+		$url = "$URI/search/issues?q=author:$User+org:$Org+type:pr" #+sort:updated+direction:desc+per_page:99"
+		Write-Verbose "$url --user ""$Username`:$Token"""
+
+		$raw = curl -s --request GET --url $url --user "$Username`:$Token" --header $Header
+		$page = $raw | ConvertFrom-Json
+
+		if ($LASTEXITCODE -ne 0) {
+			$page
+			return
+		}
+
+		Write-Host "Checking $($page.total_count) PRs for $User" -NoNewline
+		$page.items | foreach { ReportItem $_ }
+
+		Write-Host
+	}
+
+
+	function ReportItem
+	{
+		param($item)
+
+		$number = $item.number
+
+		$repo = $item.repository_url.substring($item.repository_url.lastIndexOf("/") + 1)
+
+		if ($item.draft) {
+			$state = 'Draft'
+		}
+		else {
+			$state = $item.state
+		}
+
+		$comments = $item.comments
+		$created = $item.created_at
+		if (![String]::IsNullOrWhiteSpace($created)) {
+			$created = [System.TimeZone]::CurrentTimeZone.ToLocalTime($created)
+		}
+
+		$closed = $item.closed_at
+		$days = ''
+
+		if ([String]::IsNullOrWhiteSpace($closed)) {
+			$closed = ''
+		}
+		else {
+			$closed = [System.TimeZone]::CurrentTimeZone.ToLocalTime($closed)
+			$days = (CountWeekDays $created $closed).ToString('0.##')
+		}
+
+		# examine commits
+		$u = $item.comments_url.Replace("/issues", "/pulls").Replace("/comments", "/commits")
+		$data = curl -s --request GET --url $u --user "$Username`:$Token" --header $Header | ConvertFrom-Json
+		$commits = $data.Length
+		$data | foreach { if ($_.author_association -eq 'NONE') { $comments = $comments - 1 } }
+
+		# examine comments
+		$data = curl -s --request GET --url $item.comments_url --user "$Username`:$Token" --header $Header | ConvertFrom-Json
+		$cc = 0
+		$data | foreach { $cc = $cc + $_.commit.comment_count }
+
+		"$User,$repo,$number,$state,$commits,$cc,$comments,$created,$closed,$days" | Out-File -FilePath $File -Append
+
+		Write-Host '.' -NoNewline
+	}
+}
+Process
+{
+	if ([String]::IsNullOrWhiteSpace($Org))
+	{
+		$Org = $env:GH_ORG
+		if ([String]::IsNullOrWhiteSpace($Org))
+		{
+			PromptForValue 'Your Github org'
+		}
+	}
+
+	if ([String]::IsNullOrWhiteSpace($Username))
+	{
+		$Username = $env:GH_USERNAME
+		if ([String]::IsNullOrWhiteSpace($Username))
+		{
+			PromptForValue 'Your Github username'
+		}
+	}
+
+	if ([String]::IsNullOrWhiteSpace($Token))
+	{
+		$Token = $env:GH_TOKEN
+		if ([String]::IsNullOrWhiteSpace($Token))
+		{
+			PromptForValue 'Your Github token'
+		}
+	}
+
+	if ([String]::IsNullOrWhiteSpace($User))
+	{
+		PromptForValue 'Github user to report'
+	}
+
+	$Org = [System.Web.HttpUtility]::UrlEncode($Org)
+	$User = [System.Web.HttpUtility]::UrlEncode($User)
+
+	if (!$File)
+	{
+		$File = $OutputFile
+		if (Test-Path $File)
+		{
+			Remove-Item $File -Force -Confirm:$false
+		}
+	}
+
+	InstallCurl
+
+	GetPullRequests
+}
